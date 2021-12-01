abbe211da9e272e613460b4b32e130ab4eb93549
Alan Gabriel <contatoalangabriel@gmail.com>
2021-12-01 13:32:08 -0300

    O terror dos enqueteiros foi criado

---
+++ app.js
@@ -0,0 +1,22 @@
+(() => {
+  const getPolls = () => {
+    const polls = [...document.querySelectorAll(".feed-shared-poll")];
+    return polls;
+  };
+
+  const removePolls = () => {
+    const polls = getPolls();
+
+    polls.forEach((poll) => {
+      const title = poll.offsetParent.querySelector(
+        ".feed-shared-poll__header .feed-shared-inline-show-more-text.m0"
+      ).innerText;
+      console.log(`Removendo a enquente: ${title}`);
+      poll.offsetParent.remove();
+    });
+  };
+
+  window.setInterval(function () {
+    removePolls();
+  }, 3000);
+})();
