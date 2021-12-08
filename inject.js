7beadd402c9dbf470fb86d57fd2225681cdc33a3
Alan Gabriel <contatoalangabriel@gmail.com>
2021-12-08 02:18:23 -0300

    Remove inject and activeTab permission

--- inject.js
+++
@@ -1,8 +0,0 @@
-function injectScript(file_path, tag) {
-  var node = document.getElementsByTagName(tag)[0];
-  var script = document.createElement("script");
-  script.setAttribute("type", "text/javascript");
-  script.setAttribute("src", file_path);
-  node.appendChild(script);
-}
-injectScript(chrome.extension.getURL("app.js"), "head");
